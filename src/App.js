6d4a27357eaa3a18007a0148a1c841b7834672b5
Yelin-lin <77089775+Yelin-lin@users.noreply.github.com>
2023-07-15 14:36:53 +0900

    Udpate : 구조 재설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
-import Login from './pages/Login';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 
 function App() {
   return (
