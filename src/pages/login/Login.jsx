cb6f982e481cc37c802f59c77384dccfc37292c0
Yelin-lin <77089775+Yelin-lin@users.noreply.github.com>
2023-07-17 09:26:44 +0900

    Update : style 변경

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,13 +5,13 @@ import { SLabelBox, SCenterLayout, SInputLabelContainer, SInputBox } from '../..
 function Login() {
   return (
     <SLogInLayout>
-      <SInputLabelContainer $gapSize="10px">
-        <SLogInInputLabelBox $fontSort="big">ID</SLogInInputLabelBox>
+      <SInputLabelContainer $gapSize="10px" $paddingBottom="20">
+        <SLabelBox $fontSort="big" $color="rgb(255,255,255)" $backgroundColor="rgb(255,182,193)">ID</SLabelBox>
         <SInputBox $inputMainColor="255,20,147" $inputHeight="60" $inputWidth="350" $inputFontSize="25"/>
       </SInputLabelContainer>
 
       <SInputLabelContainer $gapSize="10px">
-        <SLogInInputLabelBox $fontSort="big">PW</SLogInInputLabelBox>
+        <SLabelBox $fontSort="big" $color="rgb(255,255,255)" $backgroundColor="rgb(255,182,193)">PW</SLabelBox>
         <SInputBox $inputMainColor="255,20,147" $inputHeight="60" $inputWidth="350" $inputFontSize="25"/>
       </SInputLabelContainer>
 
@@ -24,15 +24,3 @@ export default Login
 const SLogInLayout = styled(SCenterLayout)`
   padding-top: 100px;
 `
-const SLogInInputLabelBox = styled(SLabelBox)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  color: rgb(255,255,255);
-
-  background-color: rgb(255,182,193);
-  width: 80px;
-  border-radius: 10px;
-  padding: 10px;
-`;
