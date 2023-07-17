ce42ce4988d0dafc50b00cacff0c3bf402f3addb
Yelin-lin <77089775+Yelin-lin@users.noreply.github.com>
2023-07-17 10:46:44 +0900

    Update : 로그인 뼈대, css 완료

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { styled} from 'styled-components'
-import { SLabelBox, SCenterLayout, SInputLabelContainer, SInputBox } from '../../styles/globalstyles'
+import { SLabelBox, SCenterLayout, SInputLabelContainer, SInputBox, SButtonBox } from '../../styles/globalstyles'
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  const navigate = useNavigate();
+
   return (
     <SLogInLayout>
       <SInputLabelContainer $gapSize="10px" $paddingBottom="20">
@@ -10,10 +13,17 @@ function Login() {
         <SInputBox $inputMainColor="255,20,147" $inputHeight="60" $inputWidth="350" $inputFontSize="25"/>
       </SInputLabelContainer>
 
-      <SInputLabelContainer $gapSize="10px">
+      <SInputLabelContainer $gapSize="10px" $paddingBottom="20">
         <SLabelBox $fontSort="big" $color="rgb(255,255,255)" $backgroundColor="rgb(255,182,193)">PW</SLabelBox>
         <SInputBox $inputMainColor="255,20,147" $inputHeight="60" $inputWidth="350" $inputFontSize="25"/>
       </SInputLabelContainer>
+
+      <SButtonBox $buttonSort="big" $color="rgb(255, 255, 255)" $backgroundColor="rgb(255,182,193)">로그인</SButtonBox>
+      <div>→ 회원이 아니신가요?  
+        <SLogInSpanBox onClick={()=>{
+          navigate("/join")
+        }}> 회원가입</SLogInSpanBox>
+      </div>
 
     </SLogInLayout>
   )
@@ -23,4 +33,11 @@ export default Login
 
 const SLogInLayout = styled(SCenterLayout)`
   padding-top: 100px;
+`
+
+const SLogInSpanBox = styled.span`
+  color: rgb(255,20,147);
+  font-weight: 600;
+
+  cursor: pointer;
 `
