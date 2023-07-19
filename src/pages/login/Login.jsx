5d0a357a3eab20c3cfadea094852025064e49bd4
Yelin-lin <77089775+Yelin-lin@users.noreply.github.com>
2023-07-19 11:21:16 +0900

    Update: 로그인 기능 뼈대

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,24 +1,63 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { styled} from 'styled-components'
 import { SLabelBox, SCenterLayout, SInputLabelContainer, SInputBox, SButtonBox } from '../../styles/globalstyles'
 import { useNavigate } from 'react-router-dom';
+import axios from "axios"
 
 function Login() {
   const navigate = useNavigate();
+  const [id, setId] = useState("");
+  const [password, setPassword]= useState("");
+  const [status, setStatus] = useState("아직 인증 안됨");
+  const Base_URL = "http://localhost:4000";
+
+  const handleLogin = async () => {
+    const response = await axios.post(
+      `${Base_URL}/login`,
+      {
+        "username":id,
+        "password":password,
+      },
+      {withCredentials: true}
+    );
+
+    const token = response.headers.get('Authorization');
+    localStorage.setItem("key", token);
+
+    setId('');
+    setPassword('');
+    console.log("response:", response);
+    if(response.ok){
+      setStatus("인증완료")
+    } else{
+      alert("로그인정보가 틀렸습니다")
+    }
+  };
 
   return (
     <SLogInLayout>
       <SInputLabelContainer $gapSize="10px" $paddingBottom="20">
         <SLabelBox $fontSort="big" $color="rgb(255,255,255)" $backgroundColor="rgb(255,182,193)">ID</SLabelBox>
-        <SInputBox $inputMainColor="255,20,147" $inputHeight="60" $inputWidth="350" $inputFontSize="25"/>
+        <SInputBox 
+        $inputMainColor="255,20,147" 
+        $inputHeight="60" 
+        $inputWidth="350" 
+        $inputFontSize="25"
+        value={id}
+        onChange={(e)=>setId(e.target.value)}/>
       </SInputLabelContainer>
 
       <SInputLabelContainer $gapSize="10px" $paddingBottom="20">
         <SLabelBox $fontSort="big" $color="rgb(255,255,255)" $backgroundColor="rgb(255,182,193)">PW</SLabelBox>
-        <SInputBox $inputMainColor="255,20,147" $inputHeight="60" $inputWidth="350" $inputFontSize="25"/>
+        <SInputBox 
+        $inputMainColor="255,20,147" 
+        $inputHeight="60" $inputWidth="350" 
+        $inputFontSize="25"
+        value = {password}
+        onChange = {(e)=>setPassword(e.target.value)}/>
       </SInputLabelContainer>
 
-      <SButtonBox $buttonSort="big" $color="rgb(255, 255, 255)" $backgroundColor="rgb(255,182,193)">로그인</SButtonBox>
+      <SButtonBox $buttonSort="big" $color="rgb(255, 255, 255)" $backgroundColor="rgb(255,182,193)" onClick={handleLogin}>로그인</SButtonBox>
       <div>→ 회원이 아니신가요?  
         <SLogInSpanBox onClick={()=>{
           navigate("/join")
