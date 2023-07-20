6d215bb721d27119b2acac2a1efdd658f60f9b33
yulim5002 <135326595+yulim5002@users.noreply.github.com>
2023-07-20 17:36:39 +0900

    로그인시 버튼 생성
    로그인시 새글작성 버튼 생성

--- src/pages/detailPage/DetailPage.jsx
+++ src/pages/detailPage/DetailPage.jsx
@@ -22,9 +22,15 @@ const Page = styled.div`
 
 function DetailPage() {
     const [posts, setPosts] = useState([]);
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태를 저장하는 상태
     const { shopname } = useParams();
-    //const shopnameStr = shopname.toString();
-    console.log({ shopname });
+
+    // 로그인 상태를 확인하는 함수
+    const checkLoginStatus = () => {
+        const token = localStorage.getItem('token');
+        setIsLoggedIn(!!token); // 토큰이 존재하면 isLoggedIn을 true로 설정
+    };
+
     const getPosts = async postId => {
         try {
             axios.get('http://13.125.224.157/api/posts/{postId}').then(res => {
@@ -36,6 +42,7 @@ function DetailPage() {
     };
     useEffect(() => {
         getPosts();
+        checkLoginStatus(); // 컴포넌트가 마운트될 때 로그인 상태를 확인
     }, []);
 
     const filteredPosts = posts.filter(post => post.shopname === shopname);
@@ -45,7 +52,7 @@ function DetailPage() {
         <div>
             <header>
                 <Button to="/">홈 아이콘</Button>
-                <Button to="/Post">새글 아이콘</Button>
+                {isLoggedIn ? <Button to="/Post">새글 아이콘</Button> : null}
                 <Page>게시글 상세정보</Page>
             </header>
             <div>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { styled} from 'styled-components'
-import { SLabelBox, SCenterLayout, SInputLabelContainer, SInputBox, SButtonBox } from '../../styles/globalstyles'
+import React from 'react';
+import { styled } from 'styled-components';
+import { SLabelBox, SCenterLayout, SInputLabelContainer, SInputBox, SButtonBox } from '../../styles/globalstyles';
 import { useNavigate } from 'react-router-dom';
 import { handleLogin } from '../../api/loginapi';
 import useInput from '../../hooks/useInput';
@@ -8,49 +8,57 @@ import LoginInputBox from './LoginInputBox';
 import LoginButton from './LoginButton';
 
 function Login() {
-  const navigate = useNavigate();
-  const [id, onChangeIdHandler] = useInput();
-  const [password, onChangePasswordHandler]= useInput();
+    const navigate = useNavigate();
+    const [id, onChangeIdHandler] = useInput();
+    const [password, onChangePasswordHandler] = useInput();
 
-  const handleLoginClick = async() => {
-    try{
-      const data = await handleLogin(id, password);
-        const token = data.headers.get('Authorization');
-        localStorage.setItem("token", token);
-        navigate("/");
-        console.log(data);
-    } catch(error){
-      alert("아이디와 비밀번호를 확인하세요!")
-      console.log(error)
-    }
-  };
+    const handleLoginClick = async () => {
+        try {
+            const data = await handleLogin(id, password);
+            const token = data.headers.get('Authorization');
+            localStorage.setItem('Authorization', token);
+            navigate('/');
+            console.log(data);
+        } catch (error) {
+            alert('아이디와 비밀번호를 확인하세요!');
+            console.log(error);
+        }
+    };
 
-  return (
-    <SLogInLayout>
-      <LoginInputBox value={id} onChange={onChangeIdHandler}>ID</LoginInputBox>
-      <LoginInputBox value={password} onChange={onChangePasswordHandler} type="password">PW</LoginInputBox>
+    return (
+        <SLogInLayout>
+            <LoginInputBox value={id} onChange={onChangeIdHandler}>
+                ID
+            </LoginInputBox>
+            <LoginInputBox value={password} onChange={onChangePasswordHandler} type="password">
+                PW
+            </LoginInputBox>
 
-      <LoginButton onClick={handleLoginClick}>로그인</LoginButton>
-      <div>→ 회원이 아니신가요?  
-        <SLogInSpanBox onClick={()=>{
-          navigate("/join")
-        }}> 회원가입</SLogInSpanBox>
-      </div>
-
-
-    </SLogInLayout>
-  )
+            <LoginButton onClick={handleLoginClick}>로그인</LoginButton>
+            <div>
+                → 회원이 아니신가요?
+                <SLogInSpanBox
+                    onClick={() => {
+                        navigate('/join');
+                    }}
+                >
+                    {' '}
+                    회원가입
+                </SLogInSpanBox>
+            </div>
+        </SLogInLayout>
+    );
 }
 
-export default Login
+export default Login;
 
 const SLogInLayout = styled(SCenterLayout)`
-  padding-top: 100px;
-`
+    padding-top: 100px;
+`;
 
 const SLogInSpanBox = styled.span`
-  color: #ED8936;
-  font-weight: 600;
+    color: #ed8936;
+    font-weight: 600;
 
-  cursor: pointer;
-`
+    cursor: pointer;
+`;
